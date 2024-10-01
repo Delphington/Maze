58a12cf52077c1aa05064f1b8fb84c4d4f9e820a
Delphington <Delphington@gmail.com>
2024-10-01 15:45:17 +0300

    feat: Initialization

---
+++ src/main/java/backend/academy/Maze.java
@@ -0,0 +1,13 @@
+package backend.academy;
+
+public class Maze {
+    private final int height;
+    private final int width;
+    private final Cell[][] grid;
+
+    public Maze(int height, int width, Cell[][] grid) {
+        this.height = height;
+        this.width = width;
+        this.grid = grid;
+    }
+}
