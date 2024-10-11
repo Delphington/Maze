27799b8bda6e4ed1d45b02ac8174b05ca7e3183b
Delphington <Delphington@gmail.com>
2024-10-11 16:49:50 +0300

    feat: создан отдельный класс для логики

---
+++ src/main/java/backend/academy/StartGame.java
@@ -0,0 +1,65 @@
+package backend.academy;
+
+import java.io.PrintStream;
+import java.util.List;
+
+public class StartGame {
+    private PrintStream printStream;
+
+    public StartGame(PrintStream printStream) {
+        this.printStream = printStream;
+    }
+
+    public void start() {
+
+        InputValid.inputSizeOfMaze(); // запрос ввода координат
+        InputValid.inputTypeGenerateMaze(); // запрос ввода координат
+        InputValid.voidTypeSolveMaze(); //Запрос на запрос метода решения
+
+        Generator typeMaze; //Тип генерации лабиринта
+        Maze maze; // Наш лабиринт
+        Renderer rendererMaze;
+        Solver solver;
+
+        //Генерация каким методом будем генерировать
+        if (InputValid.getTypeGenerate() == InputValid.TypeGenerate.DFS) {
+            typeMaze = new DFSMaze();
+            rendererMaze = new MazeRender();
+        } else {
+            //maze
+            typeMaze = new KruskalMaze();
+            rendererMaze = new MazeRender();
+        }
+
+        //Каким методом будем решать
+        if (InputValid.getTypeSolve() == InputValid.TypeSolve.DFS) {
+            solver = new DFSSolverMaze();
+        } else {
+            solver = new BFSSolverMaze();
+        }
+
+        //-------------------------------------
+        maze = typeMaze.generate(InputValid.getHeight(), InputValid.getWeight());
+
+        //Исходный, что будем выводить
+        String printMaze = rendererMaze.render(maze);
+        printStream.println(printMaze);
+
+        //Запрос точек
+        InputValid.inputCoordinatePoint(maze, 1);
+        InputValid.inputCoordinatePoint(maze, 2);
+
+        Coordinate point1 = InputValid.getStart();
+        Coordinate point2 = InputValid.getFinish();
+
+        //Вывод всей информации
+        //  InputValid.printInfo();
+
+        //Получали массив точек
+        List<Coordinate> path = solver.solve(maze, point1, point2);
+
+        //Печатаем Итог
+        String printMazePath = rendererMaze.render(maze, path);
+        printStream.println(printMazePath);
+    }
+}
